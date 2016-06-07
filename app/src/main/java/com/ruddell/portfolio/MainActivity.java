d2204763875555cde11bdb9cfad3651c905ce01e
Chris Ruddell <chris.ruddell@mbible.org>
2016-06-07 10:12:33 -0500

    Added layout to match suggested design from syllabus;  Added toast notifications for each button.

--- app/src/main/java/com/ruddell/portfolio/MainActivity.java
+++ app/src/main/java/com/ruddell/portfolio/MainActivity.java
@@ -1,13 +1,41 @@
 package com.ruddell.portfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final boolean DEBUG_LOG = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.popularMovies:
+                Toast.makeText(this, "This button will launch the Popular Movies app project!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.stockHawk:
+                Toast.makeText(this, "This button will launch the Stock Hawk app project!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.buildItBigger:
+                Toast.makeText(this, "This button will launch the Build it Bigger app project!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.makeAppMaterial:
+                Toast.makeText(this, "This button will launch the Make Your App Material app project!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ubiquitous:
+                Toast.makeText(this, "This button will launch the Go Ubiquitous app project!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.capstone:
+                Toast.makeText(this, "This button will launch the Capstone app project!", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
